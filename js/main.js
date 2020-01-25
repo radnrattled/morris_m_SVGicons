f9960550a9c2844ebb6bd29cc1e0de05b21c0a0c
Megan Morris <megs@Mac-mini.local>
2020-01-25 14:00:11 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 (() => {
 //module pattern
-  const theSVG = document.querySelector('.svgImg');
+  const theSVG = document.querySelectorAll('.svgImg');
   console.log('hello from JS!');
 
   let allSVGs = document.querySelectorAll(".SVG");
+
   function toggleSelected() {
     //we want to add some kind of indicator
     // that someone has selected an icon
@@ -16,7 +17,7 @@
   }
   //set up triggers
     //myspan.addEventListener('click', toggleSelected);
-    theSVG.addEventListener('click', toggleSelected);
+    theSVG.forEach(item => item.addEventListener('click', toggleSelected));
     allSVGs.forEach(item => item.addEventListener("click", logSvg));
 
 })();
